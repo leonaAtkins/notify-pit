81936ea8229c1b7138924f5ce4cd1dc9719c63c7
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2026-01-14 16:00:51 +0000

    Add account removal option.

--- lib/notify_pit.rb
+++ lib/notify_pit.rb
@@ -8,36 +8,46 @@ module NotifyPit
     configure do
       set :environment, :test
       disable :protection
+      enable :logging
+      # This ensures logs are flushed immediately to Docker logs
+      $stdout.sync = true
+    end
+
+    before do
+      # This will show up in your CodeBuild/Docker logs
+      puts "--> #{request.request_method} #{request.path}"
+      body_content = request.body&.read
+      if body_content && !body_content.empty?
+        puts "    Body: #{body_content}"
+      end
+        request.body&.rewind
     end
 
     # Use a class-level instance of the store
     DB = Store.new
 
-    helpers do
-      def json_res(data, code = 200)
-        content_type :json
-        status code
-        data.to_json
-      end
-    end
-
     post '/v2/notifications/sms' do
-      note = DB.add_notification('sms', JSON.parse(request.body.read))
+      # Read the body once and parse it
+      request_payload = JSON.parse(request.body.read)
+
+      # Pass the hash directly to the store
+      note = DB.add_notification('sms', request_payload)
+      #note = DB.add_notification('sms', JSON.parse(request.body.read))
       json_res({
-                 id: note['id'],
-                 content: { body: note['body'], from_number: 'GovWifi' },
-                 template: { id: note['template_id'], version: 1 }
-               }, 201)
+          id: note['id'],
+          content: { body: note['body'], from_number: 'GovWifi' },
+          template: { id: note['template_id'], version: 1 }
+        }, 201)
     end
 
     post '/v2/notifications/email' do
       note = DB.add_notification('email', JSON.parse(request.body.read))
       json_res({
-                 id: note['id'],
-                 content: { body: note['body'], subject: 'GovWifi details',
-                            html: "<p>#{note['body'].gsub("\n", '<br>')}</p>" },
-                 template: { id: note['template_id'], version: 1 }
-               }, 201)
+          id: note['id'],
+          content: { body: note['body'], subject: 'GovWifi details',
+                    html: "<p>#{note['body'].gsub("\n", '<br>')}</p>" },
+          template: { id: note['template_id'], version: 1 }
+        }, 201)
     end
 
     get '/v2/notifications/:id' do
@@ -61,7 +71,6 @@ module NotifyPit
 
     # Management API (For Smoke Test Orchestration)
     post '/mocker/inbound-sms' do
-      # CHANGE THIS LINE:
       msg = DB.add_inbound_sms(JSON.parse(request.body.read))
 
       json_res(msg, 201)
@@ -70,6 +79,13 @@ module NotifyPit
     get '/health' do
       status 200
       'OK'
+    end
+    private
+
+    def json_res(data, code = 200)
+      content_type :json
+      status code
+      data.to_json
     end
   end
 end
